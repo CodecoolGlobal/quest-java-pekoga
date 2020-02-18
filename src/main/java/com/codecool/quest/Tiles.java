5c61e868ead2156c40fff7eb1f4f8d20c053d46a
Kornel Leitner <leitner.kornel@gmail.com>
2020-02-18 18:34:43 +0100

    Try to create new map

--- src/main/java/com/codecool/quest/Tiles.java
+++ src/main/java/com/codecool/quest/Tiles.java
@@ -8,16 +8,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Tiles {
-    public static int TILE_WIDTH = 32;
+    public static int TILE_WIDTH = 64;
 
-    private static Image tileset = new Image("/tiles.png", 543 * 2, 543 * 2, true, false);
+    private static Image tileset = new Image("/tiles.png", 543 * 4, 543 * 4, true, false);
     private static Map<String, Tile> tileMap = new HashMap<>();
 
     public static class Tile {
         public final int x, y, w, h;
         Tile(int i, int j) {
-            x = i * (TILE_WIDTH + 2);
-            y = j * (TILE_WIDTH + 2);
+            x = i * (TILE_WIDTH + 4);
+            y = j * (TILE_WIDTH + 4);
             w = TILE_WIDTH;
             h = TILE_WIDTH;
         }
@@ -25,9 +25,9 @@ public class Tiles {
 
     static {
         tileMap.put("empty", new Tile(0, 0));
-        tileMap.put("wall", new Tile(10, 17));
+        tileMap.put("wall", new Tile(8, 0));
         tileMap.put("floor", new Tile(2, 0));
-        tileMap.put("player", new Tile(27, 0));
+        tileMap.put("player", new Tile(14, 22));
         tileMap.put("skeleton", new Tile(29, 6));
         tileMap.put("weapon", new Tile(0, 29));
     }
