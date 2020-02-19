5612aa27e47d492626fba2d0866a2a42c49f134e
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2020-02-19 14:49:43 +0100

    Set coordinates for pictures

--- src/main/java/com/codecool/quest/Tiles.java
+++ src/main/java/com/codecool/quest/Tiles.java
@@ -27,15 +27,37 @@ public class Tiles {
     }
 
     static {
-        tileMap.put("empty", new Tile(0, 0));
+        tileMap.put("road", new Tile(0, 0));
         tileMap.put("wall", new Tile(8, 0));
         tileMap.put("floor", new Tile(2, 0));
         tileMap.put("player", new Tile(14, 22));
         tileMap.put("skeleton", new Tile(29, 6));
         tileMap.put("weapon", new Tile(0, 29));
-        tileMap.put("closedDoor", new Tile(3, 9));
-        tileMap.put("openedDoor", new Tile(6, 9));
+        tileMap.put("closedDoor", new Tile(6, 16));
+        tileMap.put("openedDoor", new Tile(9, 16));
         tileMap.put("pistol", new Tile(6, 31));
+        tileMap.put("house1", new Tile(1, 20));
+        tileMap.put("house2", new Tile(1, 21));
+        tileMap.put("house3", new Tile(3, 21));
+        tileMap.put("house4", new Tile(8, 20));
+        tileMap.put("pavement1", new Tile(10, 17));
+        tileMap.put("pavement2", new Tile(16, 0));
+        tileMap.put("tree", new Tile(0, 1));
+        tileMap.put("truck1", new Tile(12, 20));
+        tileMap.put("truck2", new Tile(13, 20));
+        tileMap.put("carh1", new Tile(11, 23));
+        tileMap.put("carh2", new Tile(12, 23));
+        tileMap.put("carv1", new Tile(14, 22));
+        tileMap.put("carv2", new Tile(14, 23));
+        tileMap.put("building1", new Tile(0, 16));
+        tileMap.put("building2", new Tile(1, 16));
+        tileMap.put("building3", new Tile(2, 16));
+        tileMap.put("building4", new Tile(0, 17));
+        tileMap.put("building5", new Tile(1, 17));
+        tileMap.put("building6", new Tile(2, 17));
+        tileMap.put("building7", new Tile(0, 18));
+        tileMap.put("building8", new Tile(1, 18));
+        tileMap.put("building9", new Tile(2, 18));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
