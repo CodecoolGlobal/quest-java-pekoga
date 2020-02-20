5b7a182913557a8cd642ba3b48e583ef7b3e0b95
PeterBorok <peter.borok@gmail.com>
2020-02-20 13:48:14 +0100

    add new player icon

--- src/main/java/com/codecool/quest/logic/actors/Actor.java
+++ src/main/java/com/codecool/quest/logic/actors/Actor.java
@@ -4,6 +4,9 @@ import com.codecool.quest.logic.Cell;
 import com.codecool.quest.logic.CellType;
 import com.codecool.quest.logic.Drawable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class Actor implements Drawable {
     private static Cell cell;
     private int health = 10;
@@ -14,9 +17,31 @@ public abstract class Actor implements Drawable {
     }
 
     public void move(int dx, int dy) {
+        Set<Enum> barrierSet = new HashSet<Enum>() {{
+            add(CellType.HOUSE1);
+            add(CellType.HOUSE2);
+            add(CellType.HOUSE3);
+            add(CellType.HOUSE4);
+            add(CellType.ENEMY);
+            add(CellType.CLOSED);
+            add(CellType.CARH1);
+            add(CellType.CARV1);
+            add(CellType.CARV2);
+            add(CellType.CARH2);
+            add(CellType.TRUCK1);
+            add(CellType.BUILDING1);
+            add(CellType.BUILDING2);
+            add(CellType.BUILDING3);
+            add(CellType.BUILDING4);
+            add(CellType.BUILDING6);
+            add(CellType.BUILDING7);
+            add(CellType.BUILDING8);
+            add(CellType.BUILDING9);
+
+        }};
         Cell nextCell = cell.getNeighbor(dx, dy);
         CellType typeOfCell = nextCell.getType();
-        if (typeOfCell != CellType.HOUSE1 && typeOfCell != CellType.CARH1 && typeOfCell != CellType.CLOSED && typeOfCell != CellType.ENEMY) {
+        if (!barrierSet.contains(typeOfCell)) {
             cell.setActor(null);
             cell.setItem(null);
             nextCell.setActor(this);
