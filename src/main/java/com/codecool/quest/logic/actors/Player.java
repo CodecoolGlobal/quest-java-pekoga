f2f7ada272b148f7ad012dafc8438af6ded69b10
Kornel Leitner <leitner.kornel@gmail.com>
2020-03-03 15:28:54 +0100

    Create setPlayerTile setter for change player tile

--- src/main/java/com/codecool/quest/logic/actors/Player.java
+++ src/main/java/com/codecool/quest/logic/actors/Player.java
@@ -1,14 +1,19 @@
 package com.codecool.quest.logic.actors;
 
 import com.codecool.quest.logic.Cell;
-import com.codecool.quest.logic.actors.Actor;
 
 public class Player extends Actor {
+    String playerTile = "player";
+
     public Player(Cell cell) {
         super(cell);
     }
 
     public String getTileName() {
-        return "player";
+        return this.playerTile;
+    }
+
+    public void setPlayerTile(String playerTile) {
+        this.playerTile = playerTile;
     }
 }
